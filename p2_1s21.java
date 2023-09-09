45f75edf35f456d8644911e9afe5f82484fb2906
Cesar Giovanni Hernandez Pizano <144178558+giovas128@users.noreply.github.com>
2023-09-09 14:14:13 -0600

    Add files via upload

---
+++ p2_1s21.java
@@ -0,0 +1,11 @@
+class p2_1s21{
+public static void main(String[] args) {
+	System.out.println("Datos personales");
+	System.out.println("nombre:César Giovanni Hernández Pizano");
+	System.out.println("edad:18");
+	System.out.println("Correo:hpcesar10@gmail.com");
+	System.out.println("aficcion: ninguno");
+	System.out.println("color favorito: azul");
+
+}
+}
