b542c7866f241de95ed25da6accf96cc47885b8e
Cesar Giovanni Hernandez Pizano <144178558+giovas128@users.noreply.github.com>
2023-09-12 15:55:22 -0600

    Add files via upload

---
+++ p6_1s21.java
@@ -0,0 +1,11 @@
+class p6_1s21{
+	public static void main (String[] args){
+	String	nombre;
+	String	edad;
+	nombre = "Cesar";
+	edad = "18";
+	System.out.println("el valor paranombre es" + nombre);
+	System.out.println("la edad es " + edad);
+	System.out.println("puedes seguir programando");
+	}
+}
