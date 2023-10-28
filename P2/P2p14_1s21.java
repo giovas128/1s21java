b25a53cd86da6247ce68f62a8b6bac02973767b7
Cesar Giovanni Hernandez Pizano <144178558+giovas128@users.noreply.github.com>
2023-10-27 18:46:04 -0600

    Update P2p14_1s21.java

--- P2/P2p14_1s21.java
+++ P2/P2p14_1s21.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class p2p14_1s21{
+public class P2p14_1s21{
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
 		int[][] Ma,Mb,Mc;
-		int fa,ca,fb,cb,f,c,r;
+		int fa,ca,fb,cb,f,c,r=0,h;
 		System.out.println("ingresa el numero de filas de la Matriz A");
-		fa = teclado.nextIne();
+		fa = teclado.nextInt();
 		System.out.println("ingresa el numero de filas de la Matriz A");
 		ca = teclado.nextInt();
 		System.out.println("ingresa el numero de filas de la Matriz B");
@@ -14,9 +14,9 @@ public class p2p14_1s21{
 		System.out.println("ingresa el numero de filas de la Matriz B");
 		cb = teclado.nextInt();
 		if (ca ==fb){
-			Ma = int[fa][ca];
-			Mb = int[fb][cb];
-			Mc = int[fa][cb];
+			Ma = new int[fa][ca];
+			Mb = new int[fb][cb];
+			Mc = new int[fa][cb];
 			for (f=0;f<fa;f++) {
 				for (c=0;c<ca;c++) {
 					System.out.println("ingresa el valor para Ma["+f+"]["+c+"]: ");
@@ -32,8 +32,11 @@ public class p2p14_1s21{
             //aqui empezamos la multiplicacion
             for (f=0;f<fa;f++) {
 				for (c=0;c<cb;c++) {
-
-
+					 for (h=0;h<fb ;h++ ) {
+					 	r=Ma[f][h]*Mb[c][h];
+					 }
+					 Mc[f][c]=r;
+					 System.out.println("el resultado es" + r);
 				}
 			}
 
